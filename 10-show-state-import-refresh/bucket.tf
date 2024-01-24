ef75834215b1bf692af7cbc47e1cd4f46b31c13b
Guilherme <uchihaguilherme4@gmail.com>
2024-01-24 16:55:45 -0300

    novos conteúdos

---
+++ 10-show-state-import-refresh/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_1" {
+  bucket = "guilherme-terraform-commands-1"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "guilherme-terraform-commands-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "guilherme-terraform-commands-3"
+}
---
+++ 9-azure-vm-remote-state/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  common_tags = {
+    owner = "Guilherme Alencar"
+    managed-by = "terraform"
+  }
+}
